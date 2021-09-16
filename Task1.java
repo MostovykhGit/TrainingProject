6d4a92f3244c3c798462d1de2859b63dff30250b
MostovykhGit <90849627+MostovykhGit@users.noreply.github.com>
2021-09-16 23:00:03 +0300

    Add files via upload

--- Task1.java
+++ Task1.java
@@ -1,16 +1,29 @@
+import java.util.Arrays;
+import java.io.*;
+
+
 public class Task1
 {
     //посчитать факториал числа n
     //0.5 балла - если посчитаете в цикле
     //1 балл - если посчитаете рекурсией
     public static int fact(int n) {
-        return n;
+        if (n == 1) {
+            return n;
+        } else {
+            return n * fact(n - 1);
+        }
     }
 
     //вывести таблицу умножения на экран - 1 балл
     //подсказка - использовать двойной for
     public static void table(){
-        //Ваше решение здесь
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
     }
 
     //посчитать сумму цифр числа
@@ -18,8 +31,12 @@ public class Task1
     //для любого числа - 1 балл
     //подсказка - в случае для любого числа используйте while
     public static int sum(int n){
-        //Ваше решение здесь
-        return n;
+        int cnt = 0;
+        while (n > 0) {
+            cnt += (n % 10);
+            n /= 10;
+        }
+        return cnt;
     }
 
     //определить, является ли год високосным
@@ -30,8 +47,17 @@ public class Task1
     //Годы 2100, 2200 и 2300 - не високосные.
     //за правильный ответ - 0.5 балла
     public static boolean isLeapYear(int year) {
-        //Ваше решение здесь
-        return true;
+        if ((year % 100) == 0) {
+            if ((year % 400) == 0) {
+                return true;
+            }
+            return false;
+        } else {
+            if ((year % 4) == 0) {
+                return true;
+            }
+            return false;
+        }
     }
 
     //здесь вам нужно будет использовать результат прошлой задачи
@@ -39,9 +65,9 @@ public class Task1
     //правильный ответ - 0.5 балла
     public static int daysInYear(int year) {
         if (isLeapYear(year)){
-            //
+            System.out.println(366);
         } else {
-            //
+            System.out.println(365);
         }
         return 0;
     }
@@ -50,14 +76,31 @@ public class Task1
     //например: Понедельник - 1
     //правильный ответ - 1 балл
     public static int dayOfTheWeek(String n){
-        //Ваше решение здесь
+        if (n.equalsIgnoreCase("понедельник")) {
+            System.out.println(1);
+        } else if (n.equalsIgnoreCase("вторник")) {
+            System.out.println(2);
+        } else if (n.equalsIgnoreCase("среда")) {
+            System.out.println(3);
+        } else if (n.equalsIgnoreCase("четверг")) {
+            System.out.println(4);
+        } else if (n.equalsIgnoreCase("пятница")) {
+            System.out.println(5);
+        } else if (n.equalsIgnoreCase("суббота")) {
+            System.out.println(6);
+        } else if (n.equalsIgnoreCase("воскресенье")) {
+            System.out.println(7);
+        } else {
+            System.out.println("Это слишком");
+        }
         return 0;
     }
 
     //вывести массив на экран в виде: [1, 5, 3, 7, 10, 2, 5]
     //правильное решение - 0.5 балла
     public static void printArray(int[] array){
-        //Ваше решение здесь
+        String arrayString = Arrays.toString(array);
+        System.out.println(arrayString);
     }
 
     //вывести двойной массив на экран в виде:
@@ -66,7 +109,16 @@ public class Task1
     // ...
     //правильное решение - 0.5 балла
     public static void printArray(int[][] array){
-        //Ваше решение здесь
+        /*for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.printf("%4d", array[i][j]);
+            }
+            System.out.print("\n");
+        }*/
+        for (int i = 0; i < array.length; i++) {
+            String arrayString = Arrays.toString(array[i]);
+            System.out.println(arrayString);
+        }
     }
 
     //отсортировать одномерный массив в порядке возрастания
@@ -74,7 +126,15 @@ public class Task1
     //метод пузырька (один из самых простых для понимания)
     //правильный ответ - 1 балл
     public static int[] sort(int[] array){
-        //Ваше решение здесь
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[i]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
         return array;
     }
 
